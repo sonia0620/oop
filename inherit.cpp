ae89e1672231779c650fd15975980ce935fbd7b9
sonia0620 <71705476+sonia0620@users.noreply.github.com>
2020-12-07 17:52:34 +0800

    Add files via upload

---
+++ inherit.cpp
@@ -0,0 +1,11 @@
+/*----------------------------------*
+		 头文件：inherit.cpp
+		 功能：Founction definitions
+*-----------------------------------*/
+
+#include "INHERIT.h"
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+int base::count;//初始化
